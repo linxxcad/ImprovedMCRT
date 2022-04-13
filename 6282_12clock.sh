a06a280f915aa54cbe428440f4928056b76e84a7
linxxcad <linxxcad@zju.edu.cn>
2022-04-13 10:46:06 +0800

    master

---
+++ 6282_12clock.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+current_path=$(cd `dirname $0`; pwd)
+
+#reflection test
+# exectuable file -c json file  -s scene file  -h heliostat_index -o outfile directory
+$current_path/MCRTlxx -c InputFiles/syn/12.json -s InputFiles/syn/6282.scn -h InputFiles/syn/6282_index.txt -o OutputFiles/6282/12/
+#refraction test (the last argument is 1)
+$current_path/MCRTlxx -c InputFiles/syn/12.json -s InputFiles/syn/6282.scn -h InputFiles/syn/6282_index.txt -o OutputFiles/6282/12/ 1
+
